a1b5cf0ae78cd8f93c997a52344c88af610250a7
Christoph Witzany <christoph@web.crofting.com>
2017-12-10 14:39:01 +0100

    add none storage driver

---
+++ escapepod/scripts/storage/none/get-backup-metadata.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source $(dirname "$0")/variables.sh
+mkdir -p ${METADATA_PATH}
+if [ -z "$1" ]; then
+  BACKUP_ID=$(ls -lrc --color=never $METADATA_PATH | awk '{print $(NF)}' | grep "metadata$" | tail -n1)
+else
+  BACKUP_ID=$1.metadata
+fi
+
+if [ -f ${METADATA_PATH}/${BACKUP_ID} ]; then
+  cat ${METADATA_PATH}/${BACKUP_ID}
+else
+  echo "no-remote-metadata"
+fi
